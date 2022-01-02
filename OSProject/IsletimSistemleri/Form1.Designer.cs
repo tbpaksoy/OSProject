23ae69cdd655811b70a35c3b30523f59d4192774
Kerem <65291634+keremduman30@users.noreply.github.com>
2022-01-02 13:24:47 +0300

    Kerem Duman

---
+++ OSProject/IsletimSistemleri/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace IsletimSistemleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_klavye_arayuz = new System.Windows.Forms.Label();
+            this.txt_ram_hafiza = new System.Windows.Forms.Label();
+            this.txt_islemci_hizi = new System.Windows.Forms.Label();
+            this.txt_islemci_adi = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_cekirdek = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_klavye_arayuz
+            // 
+            this.txt_klavye_arayuz.AutoSize = true;
+            this.txt_klavye_arayuz.Location = new System.Drawing.Point(60, 357);
+            this.txt_klavye_arayuz.Name = "txt_klavye_arayuz";
+            this.txt_klavye_arayuz.Size = new System.Drawing.Size(46, 17);
+            this.txt_klavye_arayuz.TabIndex = 19;
+            this.txt_klavye_arayuz.Text = "label6";
+            // 
+            // txt_ram_hafiza
+            // 
+            this.txt_ram_hafiza.AutoSize = true;
+            this.txt_ram_hafiza.Location = new System.Drawing.Point(57, 296);
+            this.txt_ram_hafiza.Name = "txt_ram_hafiza";
+            this.txt_ram_hafiza.Size = new System.Drawing.Size(46, 17);
+            this.txt_ram_hafiza.TabIndex = 18;
+            this.txt_ram_hafiza.Text = "label6";
+            // 
+            // txt_islemci_hizi
+            // 
+            this.txt_islemci_hizi.AutoSize = true;
+            this.txt_islemci_hizi.Location = new System.Drawing.Point(54, 221);
+            this.txt_islemci_hizi.Name = "txt_islemci_hizi";
+            this.txt_islemci_hizi.Size = new System.Drawing.Size(46, 17);
+            this.txt_islemci_hizi.TabIndex = 17;
+            this.txt_islemci_hizi.Text = "label6";
+            // 
+            // txt_islemci_adi
+            // 
+            this.txt_islemci_adi.AutoSize = true;
+            this.txt_islemci_adi.Location = new System.Drawing.Point(51, 162);
+            this.txt_islemci_adi.Name = "txt_islemci_adi";
+            this.txt_islemci_adi.Size = new System.Drawing.Size(46, 17);
+            this.txt_islemci_adi.TabIndex = 16;
+            this.txt_islemci_adi.Text = "label2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label5.Location = new System.Drawing.Point(50, 316);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(153, 24);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Klavye Arayüzü";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Location = new System.Drawing.Point(50, 251);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 24);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Ram Hafızası";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(50, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 24);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "İşlemci Hızı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(50, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 24);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "İşlemci Adi";
+            // 
+            // txt_cekirdek
+            // 
+            this.txt_cekirdek.AutoSize = true;
+            this.txt_cekirdek.Location = new System.Drawing.Point(51, 73);
+            this.txt_cekirdek.Name = "txt_cekirdek";
+            this.txt_cekirdek.Size = new System.Drawing.Size(0, 17);
+            this.txt_cekirdek.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(47, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 24);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Çekirdek Sayisi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_klavye_arayuz);
+            this.Controls.Add(this.txt_ram_hafiza);
+            this.Controls.Add(this.txt_islemci_hizi);
+            this.Controls.Add(this.txt_islemci_adi);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_cekirdek);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txt_klavye_arayuz;
+        private System.Windows.Forms.Label txt_ram_hafiza;
+        private System.Windows.Forms.Label txt_islemci_hizi;
+        private System.Windows.Forms.Label txt_islemci_adi;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label txt_cekirdek;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
