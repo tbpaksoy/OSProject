fb38856db60c1d622293a37408a83ce5f822fa9e
Brahim01124 <95984037+Brahim01124@users.noreply.github.com>
2022-01-07 12:12:10 +0300

    usain and brahim

---
+++ Son Hali/OSProject-main/OSProject/Form1.Designer.cs
@@ -0,0 +1,392 @@
+﻿
+namespace OSProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.label23 = new System.Windows.Forms.Label();
+            this.label24 = new System.Windows.Forms.Label();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label25 = new System.Windows.Forms.Label();
+            this.label26 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label27 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage3.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.label24);
+            this.tabPage3.Controls.Add(this.label23);
+            this.tabPage3.Controls.Add(this.label22);
+            this.tabPage3.Controls.Add(this.label21);
+            this.tabPage3.Controls.Add(this.label18);
+            this.tabPage3.Controls.Add(this.label19);
+            this.tabPage3.Controls.Add(this.label10);
+            this.tabPage3.Controls.Add(this.label9);
+            this.tabPage3.Controls.Add(this.label11);
+            this.tabPage3.Controls.Add(this.label12);
+            this.tabPage3.Location = new System.Drawing.Point(4, 29);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(573, 436);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Hafıza";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label12.Location = new System.Drawing.Point(54, 46);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(82, 28);
+            this.label12.TabIndex = 4;
+            this.label12.Text = "label12";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label11.Location = new System.Drawing.Point(54, 102);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(82, 28);
+            this.label11.TabIndex = 5;
+            this.label11.Text = "label11";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(54, 158);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(70, 28);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label10.Location = new System.Drawing.Point(54, 214);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(82, 28);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "label18";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label19.Location = new System.Drawing.Point(54, 270);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(82, 28);
+            this.label19.TabIndex = 9;
+            this.label19.Text = "label19";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(54, 138);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(64, 20);
+            this.label18.TabIndex = 11;
+            this.label18.Text = "Disk Adı";
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Location = new System.Drawing.Point(54, 194);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(135, 20);
+            this.label21.TabIndex = 12;
+            this.label21.Text = "Disk Bölümlenmesi";
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Location = new System.Drawing.Point(54, 82);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(94, 20);
+            this.label22.TabIndex = 13;
+            this.label22.Text = "Disk Üreticisi";
+            // 
+            // label23
+            // 
+            this.label23.AutoSize = true;
+            this.label23.Location = new System.Drawing.Point(54, 26);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(88, 20);
+            this.label23.TabIndex = 14;
+            this.label23.Text = "Disk Modeli";
+            // 
+            // label24
+            // 
+            this.label24.AutoSize = true;
+            this.label24.Location = new System.Drawing.Point(54, 250);
+            this.label24.Name = "label24";
+            this.label24.Size = new System.Drawing.Size(136, 20);
+            this.label24.TabIndex = 15;
+            this.label24.Text = "Disk boyutU (BYTE)";
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label27);
+            this.tabPage1.Controls.Add(this.label16);
+            this.tabPage1.Controls.Add(this.label15);
+            this.tabPage1.Controls.Add(this.label14);
+            this.tabPage1.Controls.Add(this.label13);
+            this.tabPage1.Controls.Add(this.label26);
+            this.tabPage1.Controls.Add(this.label25);
+            this.tabPage1.Controls.Add(this.label20);
+            this.tabPage1.Controls.Add(this.label17);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(573, 436);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "CPU";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(33, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bellek Boyutu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(33, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Yuva Sayısı";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(33, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(171, 28);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Kullanılan Bellek";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(33, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 28);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Hız";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(33, 33);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(177, 20);
+            this.label17.TabIndex = 9;
+            this.label17.Text = "İşlemcinin Çekirdek Sayısı";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(33, 145);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(82, 20);
+            this.label20.TabIndex = 10;
+            this.label20.Text = "İşlemci Adı";
+            // 
+            // label25
+            // 
+            this.label25.AutoSize = true;
+            this.label25.Location = new System.Drawing.Point(33, 201);
+            this.label25.Name = "label25";
+            this.label25.Size = new System.Drawing.Size(179, 20);
+            this.label25.TabIndex = 11;
+            this.label25.Text = "En Fazla İşlemci Saati Hızı";
+            // 
+            // label26
+            // 
+            this.label26.AutoSize = true;
+            this.label26.Location = new System.Drawing.Point(33, 89);
+            this.label26.Name = "label26";
+            this.label26.Size = new System.Drawing.Size(155, 20);
+            this.label26.TabIndex = 12;
+            this.label26.Text = "Matıksal İşlemci Sayısı";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label13.Location = new System.Drawing.Point(33, 277);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(41, 28);
+            this.label13.TabIndex = 13;
+            this.label13.Text = "99:";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label14.Location = new System.Drawing.Point(74, 277);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(41, 28);
+            this.label14.TabIndex = 14;
+            this.label14.Text = "99:";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label15.Location = new System.Drawing.Point(109, 277);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(41, 28);
+            this.label15.TabIndex = 15;
+            this.label15.Text = "99:";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label16.Location = new System.Drawing.Point(152, 277);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(36, 28);
+            this.label16.TabIndex = 16;
+            this.label16.Text = "99";
+            // 
+            // label27
+            // 
+            this.label27.AutoSize = true;
+            this.label27.Location = new System.Drawing.Point(33, 257);
+            this.label27.Name = "label27";
+            this.label27.Size = new System.Drawing.Size(115, 20);
+            this.label27.TabIndex = 17;
+            this.label27.Text = "Çalışma Zamanı";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(2, 15);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(581, 469);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 496);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label label24;
+        private System.Windows.Forms.Label label23;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label27;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label26;
+        private System.Windows.Forms.Label label25;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+    }
+}
+
---
+++ Son Hali/OSProject-main/OSProject/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using Microsoft.VisualBasic.Devices;
+
+namespace OSProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            HardwareInfo hardware = new HardwareInfo();
+
+            hardware.AddSearchTerms("NumberOfCores" , "Win32_Processor");
+            /*hardware.AddSearchTerms("Name", "Win32_Processor");
+            hardware.AddSearchTerms("MaxClockSpeed", "Win32_Processor");
+            hardware.AddSearchTerms("NumberOfLogicalProcessors", "Win32_Processor");
+            hardware.AddSearchTerms("Layout", "Win32_Keyboard");*/
+            string[] asd = hardware.GetSearchTerms();
+            //label3.Text = hardware.AddSearchTerms("NumberOfCores", "Win32_Processor");
+            //label3.Text = null;
+            /*foreach(string s in asd) 
+            {
+                label3.Text += s + "\n";
+            }*/
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HardwareInfo ram = new HardwareInfo();
+
+            ram.AddSearchTerms("NumberOfCores", "Win32_Processor");//İşlemcinin çekirdek sayısı
+            ram.AddSearchTerms("Name", "Win32_Processor");//İşlemci Adı
+            ram.AddSearchTerms("MaxClockSpeed", "Win32_Processor");//En fazla işlemci saati hızı
+            ram.AddSearchTerms("NumberOfLogicalProcessors", "Win32_Processor");//Matıksal İşlemci Sayısı
+            label1.Text = null;
+            label2.Text = null;
+            label3.Text = null;
+            label4.Text = null;
+            string[] asd = ram.GetSearchTerms();
+            label1.Text += asd[0] + "\n";
+            label2.Text += asd[1] + "\n";
+            label3.Text += asd[2] + "\n";
+            label4.Text += asd[3] + "\n";
+
+
+
+            HardwareInfo hardware2 = new HardwareInfo();
+
+            hardware2.AddSearchTerms("Layout", "Win32_Keyboard");//Klavye arayüzü
+            label8.Text = null;
+            string[] asd2 = hardware2.GetSearchTerms();
+            label8.Text += asd2[0] + "\n";
+            
+
+            HardwareInfo storage = new HardwareInfo();
+            storage.AddSearchTerms("Name", "Win32_DiskDrive");//Disk Adı 9
+            storage.AddSearchTerms("Model", "Win32_DiskDrive");//Disk Modeli 10
+            storage.AddSearchTerms("Manufacturer", "Win32_DiskDrive");//Disk üretici 11
+            storage.AddSearchTerms("Partitions", "Win32_DiskDrive");//Disk bölümlenmesi 12
+            storage.AddSearchTerms("Size", "Win32_DiskDrive");//Disk boyutı(byte) 19
+            
+            label9.Text = null;
+            label10.Text = null;
+            label11.Text = null;
+            label12.Text = null;
+            label19.Text = null;
+
+            string[] storageData = storage.GetSearchTerms();
+            label9.Text += storageData[0] + "\n";
+            label10.Text += storageData[1] + "\n";
+            label11.Text += storageData[2] + "\n";
+            label12.Text += storageData[3] + "\n";
+            label19.Text += storageData[4] + "\n";
+
+
+            label5.Text = PC_NeZamandirAcik();
+            PC_NeZamandirAcik();
+
+
+        }
+        //Ne kada süredir çalıştığı
+        int gun = Environment.TickCount / 86400000;
+        int saat = Environment.TickCount / 3600000 % 24;
+        int dakika = Environment.TickCount / 120000 % 60;
+        int saniye = Environment.TickCount / 1000 % 60;
+        int salise = 0;
+        public string PC_NeZamandirAcik()
+        {
+            
+
+
+            String cevap = String.Empty;
+
+            cevap += Convert.ToString(gun) + " gün, ";
+            cevap += Convert.ToString(saat) + " saat, ";
+            cevap += Convert.ToString(dakika) + " dakika, ";
+            cevap += Convert.ToString(saniye) + " saniye.";
+
+            timer1.Start();
+
+            return cevap;
+
+        }
+        
+
+        private uint GetCurrentCPUClock() 
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("select CurrentClockSpeed from Win32_Processor");
+            foreach(var item in searcher.Get()) 
+            {
+                return (uint)item["CurrentClockSpeed"];
+            }
+            return 0;
+        }
+        private void UpdateCurentClockLabel(object sender, EventArgs e) 
+        {
+
+        }
+        //Zaman hesaplayıcı
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            saniye++;
+            //progressBar1.Value = salise;
+            if (saniye == 60)
+            {
+                saniye = 0;
+                dakika++;
+            }
+
+            if (dakika == 60)
+            {
+                dakika = 0;
+                saat++;
+            }
+
+
+            if (saat == 24)
+            {
+                saat = 0;
+                gun++;
+            }
+
+            label16.Text = saniye.ToString()+   ":";
+            label15.Text = dakika.ToString()+   ":";
+            label14.Text = saat.ToString()+     ":";
+            label13.Text = gun.ToString();
+        }
+    }
+}
